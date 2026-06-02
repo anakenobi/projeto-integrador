ab5f5c58beb0636e15acb920e8f0e00ab522d2a5
Eduarda Macedo <eduardamac75@gmail.com>
2026-06-02 20:38:33 -0300

    primeiro commit

---
+++ src/test/java/projeto_integrador/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto_integrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
